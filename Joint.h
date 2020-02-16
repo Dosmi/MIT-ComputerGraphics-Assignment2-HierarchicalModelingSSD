5258f5e3ced8b04f61afc9de26580476dc8579d9
sc17dd <dom@localhost.localdomain>
2020-02-16 16:02:24 +0000

    added additional joint information: the parent index and the initial transform to reference by the bone rotation

--- Joint.h
+++ Joint.h
@@ -6,8 +6,10 @@
 
 struct Joint
 {
-	int index;
-	Matrix4f transform; // transform relative to its parent
+	int index;											// index of the joint as given in the .skel file
+	int parent;								 			// added index of each joint's parent
+	Matrix4f transform;				 			// transform relative to its parent (dynamic)
+	Matrix4f initialTransform; 			// used to save the original transform relative to its parent (const reference matrix)
 	std::vector< Joint* > children; // list of children
 
 	// This matrix transforms world space into joint space for the initial ("bind") configuration of the joints.

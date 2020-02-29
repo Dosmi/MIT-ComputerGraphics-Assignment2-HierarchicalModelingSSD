9801356dabd00225c2df4f9cd8b2aa0a253470fc
sc17dd <dom@localhost.localdomain>
2020-02-29 20:25:33 +0000

    finished writing up Matrix Stack

--- MatrixStack.cpp
+++ MatrixStack.cpp
@@ -3,15 +3,10 @@
 // Implemented 2019-12-01
 MatrixStack::MatrixStack()
 {
-	std::cout << "Initialize the matrix stack with the identity matrix." << std::endl;
 	// Initialize the matrix stack with the identity matrix.
 	Matrix4f identity_matrix(1);
 	identity_matrix = identity_matrix.identity();
 	m_matrices.push_back(identity_matrix);
-
-	// Matrix4f test = m_matrices.back();
-	// test.print();
-
 }
 
 void MatrixStack::print()
@@ -34,41 +29,29 @@ void MatrixStack::print()
 void MatrixStack::clear()
 {
 	// Revert to just containing the identity matrix.
-	// std::cout << "called to clear " << std::endl;
-	// this->print();
 	m_matrices.clear();
-	// this->print();
 
 	Matrix4f identity_matrix(1);
 	identity_matrix = identity_matrix.identity();
 	m_matrices.push_back(identity_matrix);
-
-	// this->print();
 }
 
 Matrix4f MatrixStack::top()
 {
 	// Return the top of the stack
 	Matrix4f top_transformation = m_matrices.back();
-	// std::cout << "getting back: \n";
-	// this->print();
-
 	return top_transformation;
 }
 
 void MatrixStack::push( const Matrix4f& m )
 {
 	// Push m onto the stack.
-	std::cout << "Pushin .. . . \n";
 	Matrix4f top_transformation = m_matrices.back();
 
 	// Your stack should have OpenGL semantics:
 	// the new top should be the old top multiplied by m
 	Matrix4f related_to_parent = top_transformation * m;
 	m_matrices.push_back(related_to_parent);
-
-	// this->print();
-
 }
 
 void MatrixStack::pop()
